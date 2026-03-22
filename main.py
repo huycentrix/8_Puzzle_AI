e806a8b6e7646e44bb45313aa994f8d5af150a18
Cao Hai Vy <vycao1203@gmail.com>
2026-03-22 20:00:39 +0700

    Add IDA*

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from puzzle.puzzle import Puzzle
 from algorithms.greedy import GreedyBestFirstSearch
+from algorithms.idastar import IDAStarSearch
 
 start = (
-    (1,2,3),
-    (4,0,6),
-    (7,5,8)
+    (8,6,7),
+    (2,5,4),
+    (3,0,1)
 )
 
 goal = (
@@ -15,28 +16,63 @@ goal = (
 
 puzzle = Puzzle(goal)
 
-algo = GreedyBestFirstSearch(start, goal, puzzle)
-result = algo.search()
+if not puzzle.is_solvable(start):
+    print("Error: Unsolvenable problem!")
+else:
 
-for i, step in enumerate(result.steps):
-    print(f"\n===== Step {i+1} =====")
+    # algo = IDAStarSearch(start, goal, puzzle)
+    # result = algo.search()
 
-    print("Current:")
-    for row in step["current"]:
-        print(row)
+    # # in step-by-step
+    # for i, step in enumerate(result.steps):
+    #     print(f"\n===== Step {i+1} =====")
 
-    print("h(current) =", step["h"])
+    #     print("Current:")
+    #     for row in step["current"]:
+    #         print(row)
 
-    if step["is_goal"]:
-        print("GOAL FOUND!")
+    #     print(f"g={step['g']}, h={step['h']}, f={step['f']}, f_limit={step['f_limit']}")
 
-    print("\nFrontier:")
-    for node in step["frontier"]:
-        print("State:")
-        for row in node["state"]:
+    #     if step["is_goal"]:
+    #         print("GOAL FOUND!")
+
+    #     print("\nFrontier (successors):")
+    #     for node in step["frontier"]:
+    #         print("State:")
+    #         for row in node["state"]:
+    #             print(row)
+
+    #         print(f"g={node['g']}, h={node['h']}, f={node['f']}")
+
+    #         # highlight nếu bị cắt
+    #         if node["f"] > step["f_limit"]:
+    #             print("CUT OFF (f > f_limit)")
+
+    #         print("------")
+
+    algo = GreedyBestFirstSearch(start, goal, puzzle)
+    result = algo.search()
+
+    for i, step in enumerate(result.steps):
+        print(f"\n===== Step {i+1} =====")
+
+        print("Current:")
+        for row in step["current"]:
             print(row)
-        print("h =", node["h"])
-        print("------")
 
-print("Path:", result.path)
-print("Steps:", len(result.path))
+        print("h(current) =", step["h"])
+
+        if step["is_goal"]:
+            print("GOAL FOUND!")
+
+        print("\nFrontier:")
+        for node in step["frontier"]:
+            print("State:")
+            for row in node["state"]:
+                print(row)
+            print("h =", node["h"])
+            print("------")
+
+    print("\nFinal Path:", result.path)
+    print("Steps:", len(result.path) - 1)
+
